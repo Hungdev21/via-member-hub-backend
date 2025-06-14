7e7f28e3318f125df187a857f8e437dd67effff7
Hungdev21 <phamviethung21052004@gmail.com>
2025-06-14 20:56:54 +0700

    fix middleware

--- backend/routes/tokenRouter.js
+++ backend/routes/tokenRouter.js
@@ -1,6 +1,9 @@
 const tokenController = require('../controllers/token.controller');
 const express = require('express');
 const router = express.Router();
-const middleware=require('../middleware/auth.middleware');
-router.get('/gettoken',middleware.checkTokenFE(),tokenController.getToken);
+// const middleware=require('../middleware/auth.middleware');
+// router.get('/gettoken',middleware.checkTokenFE(),tokenController.getToken);
+router.get('/gettoken',tokenController.getToken);
+
+
 module.exports=router
